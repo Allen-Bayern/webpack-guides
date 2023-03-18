f2f87f4d8789beb516dca3ff86965e304ff89eaf
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-03-18 13:49:08 +0800

    feat: Asset Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,63 @@
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            // load css
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            // load pics
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                type: 'asset/resource',
+            },
+            // load fonts
+            {
+                test: /\.(woff2?|eot|[ot]tf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.[ct]sv$/i,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.toml$/i,
+                type: 'json',
+                parser: {
+                    parse: toml.parse,
+                }
+            },
+            {
+                test: /\.ya?ml$/i,
+                type: 'json',
+                parser: {
+                    parse: yaml.parse,
+                }
+            },
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                }
+            },
+        ],
     },
 };
